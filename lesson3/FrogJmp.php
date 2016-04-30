d9c510aaa5103f8d53cc1de88ff08284930ecdb3
Stephane Martin-Dupont <smartindupont@gmail.com>
2016-05-01 02:24:31 +0800

    add FromJmp

---
+++ lesson3/FrogJmp.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @param integer $X frog position
+ * @param integer $Y frog minimal destination
+ * @param integer $D frog jump distance
+ * @return integer
+ *
+ * @see https://codility.com/demo/take-sample-test/frog_jmp/
+ * @see https://codility.com/demo/results/trainingV32U8J-3VU/
+ */
+function solution($X, $Y, $D) {
+  return intval(ceil(($Y - $X) / $D));
+}
